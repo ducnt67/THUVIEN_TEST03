09f6dcb12b99b8e09bddec083641e0d2e96bb005
tiendung-nguyent <nguyenthitiendung@gmail.com>
2026-04-15 03:26:17 +0700

    sua cau truc

--- THUVIENDUE_03/settings.py
+++ THUVIENDUE_03/settings.py
@@ -36,6 +36,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'library_system',
+    'muon_sach',
+    'tra_sach',
+    'quan_ly_sach',
+    'quan_ly_nguoi_dung',
+    'bao_cao',
+    'quan_ly_tai_khoan',
+    'tong_quan',
 ]
 
 MIDDLEWARE = [
--- THUVIENDUE_03/urls.py
+++ THUVIENDUE_03/urls.py
@@ -3,5 +3,11 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('library_system.urls')),
+    path('', include('tong_quan.urls')),
+    path('', include('quan_ly_tai_khoan.urls')),
+    path('', include('muon_sach.urls')),
+    path('', include('tra_sach.urls')),
+    path('', include('quan_ly_sach.urls')),
+    path('', include('quan_ly_nguoi_dung.urls')),
+    path('', include('bao_cao.urls')),
 ]
